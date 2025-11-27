da2fa835af9dc4f299d87685efd24ab78348fc6b
oriiquesillva <l.sillvasouza@gmail.com>
2025-11-27 14:23:23 -0300

    feat: add product list and product item components

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,43 @@
 import Header from "@/components/common/header";
+import ProductList from "@/components/common/product-list";
 import { Button } from "@/components/ui/button";
+import { db } from "@/db";
+import Image from "next/image";
 
-const Home = () => {
+const Home = async () => {
+  const products = await db.query.productTable.findMany({
+    with: {
+      variants: true,
+    },
+  });
   return (
     <>
       <Header />
+      <div className="space-y-6">
+        <div className="px-5">
+          <Image
+            className="h-auto w-full"
+            src="/banner1.png"
+            alt="Estilo além da imaginação"
+            height={0}
+            width={0}
+            sizes="100vw"
+          />
+        </div>
+
+        <ProductList title="Os mais vendidos" products={products} />
+
+        <div className="px-5">
+          <Image
+            className="h-auto w-full"
+            src="/banner2.png"
+            alt="Estilo além da imaginação"
+            height={0}
+            width={0}
+            sizes="100vw"
+          />
+        </div>
+      </div>
     </>
   );
 };
